67e803025a1ee6842d0478cab3bf4ba811dc2539
Bartłomiej Skorupski <bartmann001@gmail.com>
2024-03-14 18:37:42 +0100

    Custom loading spinner

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,19 @@
 import { useContext } from 'react';
 import WeatherContext from '../context/weather-context';
+import LoadingSpinner from './UI/LoadingSpinner';
 
 export default function Header() {
-  const { location, current } = useContext(WeatherContext);
+  const { location, current, loading } = useContext(WeatherContext);
 
   return (
     <header className="text-white text-center pt-16 mb-16">
       <h1 className="font-semibold drop-shadow-md">{location}</h1>
-      <p className="text-9xl font-light drop-shadow-md pl-12">
-        {current?.temperature_2m?.toFixed(0)}&deg;
-      </p>
+      {loading && <LoadingSpinner className="size-32 rotate-90" />}
+      {!loading && (
+        <p className="text-9xl font-light drop-shadow-md pl-12">
+          {current?.temperature_2m?.toFixed(0)}&deg;
+        </p>
+      )}
     </header>
   );
 }
---
+++ src/components/UI/LoadingSpinner.tsx
@@ -0,0 +1,23 @@
+import styles from './LoadingSpinner.module.css';
+
+type LoadingSpinnerProps = {
+  className?: string;
+};
+
+function SpinnerDot() {
+  return (
+    <div className="absolute size-1/5 bg-white rounded-full top-0 left-1/2 -translate-x-1/2"></div>
+  );
+}
+
+export default function LoadingSpinner({ className }: LoadingSpinnerProps) {
+  return (
+    <div className={`${className} relative inline-block size-8`}>
+      {[...Array(5)].map((_, i) => (
+        <div key={i} className={`absolute size-full top-0 ${styles.dotSpin}`}>
+          <SpinnerDot />
+        </div>
+      ))}
+    </div>
+  );
+}
